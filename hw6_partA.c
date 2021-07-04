d6735339a6aeacdfd2c0ff2936b78ef808ac5d91
Rabeya123 <74776091+Rabeya123@users.noreply.github.com>
2021-07-04 21:21:36 +0600

    Update hw6_partA.c

--- hw6_partA.c
+++ hw6_partA.c
@@ -1,12 +1,3 @@
-/*Programmer Chase Singhofen
-Date 10/18/16
-Specifications: Write a program that will Create a
-loop that will output all the multiples of 5
-that are greater than zero and less than 60 (do not include 60)
-*/
-
-
-
 #include<stdio.h>
 #include<stdlib.h>
 
